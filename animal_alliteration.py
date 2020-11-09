d4f81b4e17b5978f4ca7bea7a0bde7ee0b84f776
Markus Kreitzer <atlassian@fltr.pw>
2020-11-09 18:30:45 -0500

    Cleaned up and added some type hints.

--- animal_alliteration.py
+++ animal_alliteration.py
@@ -1,18 +1,19 @@
 from random import choice
+from typing import List
+
 __author__ = 'Markus - elec3647'
 
-alpha = 'abcdefghijklmnopqrstuvwxyz'
+alpha: str = 'abcdefghijklmnopqrstuvwxyz'
 
 with open('adjectives.txt') as adj:
     with open('animals.txt') as ani:
-        adjectives = adj.readlines()
-        animals = ani.readlines()
+        adjectives: List[str] = adj.readlines()
+        animals: List[str] = ani.readlines()
         letter = choice(alpha)
-        reg1 = [line.strip() for line in adjectives if letter == line[0].lower()]
-        reg2 = [line.strip() for line in animals if letter == line[0].lower()]
+        reg1: List[str] = [line.strip() for line in adjectives if letter == line[0].lower()]
+        reg2: List[str] = [line.strip() for line in animals if letter == line[0].lower()]
         if len(reg1) == 0 or len(reg2) == 0:
             pass
         else:
-            boo ='%s %s' % (choice(reg1), choice(reg2))
-            print(boo.title())
-
+            completed_string: str = '%s %s' % (choice(reg1), choice(reg2))
+            print(completed_string.title())
