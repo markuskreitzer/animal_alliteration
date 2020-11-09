c723374cb9528f2c64f0b45298717193ed5fa679
Markus Kreitzer <atlassian@fltr.pw>
2020-11-09 18:17:37 -0500

    Quick, Fun Script

---
+++ animal_alliteration.py
@@ -0,0 +1,19 @@
+import json
+import re
+from random import choice
+
+alpha = 'abcdefghijklmnopqrstuvwxyz'
+
+with open('adjectives.txt') as adj:
+    with open('animals.txt') as ani:
+        adjectives = adj.readlines()
+        animals = ani.readlines()
+        letter = choice(alpha)
+        reg1 = [line.strip() for line in adjectives if letter == line[0].lower()]
+        reg2 = [line.strip() for line in animals if letter == line[0].lower()]
+        if len(reg1) == 0 or len(reg2) == 0:
+            pass
+        else:
+            boo ='%s %s' % (choice(reg1), choice(reg2))
+            print(boo.title())
+
